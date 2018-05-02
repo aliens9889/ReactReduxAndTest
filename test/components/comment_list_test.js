524ad7f13ac891741b923efe1b15735f9e2d139f
Alonsop <alonsop@uakami.com>
2018-05-02 16:53:06 -0400

    Testing the commentList component

---
+++ test/components/comment_list_test.js
@@ -0,0 +1,22 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+describe('Comment List', () => {
+
+  let component;
+
+  beforeEach(() => {
+    const props = { comments: ['new comment', 'new comment two']};
+    component = renderComponent(CommentList, null, props);
+  });
+
+  it('shows an LI for each comment', () => {
+    expect(component.find('li').length).to.equal(2);
+  });
+
+  it('shows each comment that is provided', () => {
+    expect(component).to.contain('new comment');
+    expect(component).to.contain('new comment two');
+  });
+
+});
